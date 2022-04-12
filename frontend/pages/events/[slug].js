0122a7ef3f6d86fda5b6744cf76f2910f64a19f2
Leo <lcheung@swellbottle.com>
2022-04-12 13:55:09 -0400

    set up event detail page

--- frontend/pages/events/[slug].js
+++ frontend/pages/events/[slug].js
@@ -1,11 +1,54 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index'
+import styles from '@/styles/Event.module.css'
 
 export default function EventPage({ evt }) {
+  const deleteEvent = (e) => {
+    e.preventDefault()
+    console.log(e)
+  }
   return (
     <Layout title="Event Page">
-      <h1>{evt.name}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes />
+          </a>
+        </div>
+
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={`${evt.image}`} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+
+        <h3>Description</h3>
+        <p>{evt.description}</p>
+
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{`< Go back`}</a>
+        </Link>
+      </div>
     </Layout>
   )
 }
@@ -20,7 +63,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: true
+    fallback: true,
   }
 }
 
@@ -34,7 +77,7 @@ export async function getStaticProps({ params: { slug } }) {
     props: {
       evt: events[0],
     },
-    revalidate: 1
+    revalidate: 1,
   }
 }
 
