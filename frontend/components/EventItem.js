22239ca73ffc5ed9c90ce16ad0c691a116574b85
Leo <lcheung@swellbottle.com>
2022-04-12 17:48:29 -0400

    using strapi api response json

--- frontend/components/EventItem.js
+++ frontend/components/EventItem.js
@@ -7,19 +7,20 @@ export default function EventItem({ evt }) {
     <div className={styles.event}>
       <div className={styles.img}>
         <Image
-          src={evt.image ? evt.image : '/images/event-default.png'}
+          src={evt.attributes.image ? evt.attributes.image.data.attributes.formats.thumbnail.url : '/images/event-default.png'}
           width={170}
           height={100}
+          alt={evt.attributes.name}
         />
       </div>
       
       <div className={styles.info}>
-          <span>{evt.date} at {evt.time}</span>
-          <h3>{evt.name}</h3>
+          <span>{evt.attributes.date} at {evt.attributes.time}</span>
+          <h3>{evt.attributes.name}</h3>
       </div>
 
         <div className={styles.link}>
-            <Link href={`/events/${evt.slug}`}>
+            <Link href={`/events/${evt.attributes.slug}`}>
                 <a className='btn'>Details</a>
             </Link>
         </div>
