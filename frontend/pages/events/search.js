bb8ed1d555aed1692ea444262a5bc6f298ed7f20
Leo <lcheung@swellbottle.com>
2022-04-12 18:46:38 -0400

    add search result page

---
+++ frontend/pages/events/search.js
@@ -0,0 +1,56 @@
+import Link from 'next/link'
+import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
+import qs from 'qs'
+import { useRouter } from 'next/router'
+
+export default function SearchPage({ events }) {
+  const router = useRouter()
+
+  return (
+    <Layout title="Search Results">
+      <Link href="/events">{`< Go Back`}</Link>
+      <h1>Search Results for "{router.query.term}"</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
+    </Layout>
+  )
+}
+
+// use server side props
+export async function getServerSideProps({ query: { term } }) {
+  const query = qs.stringify(
+    {
+      filters: {
+        $or: [
+          { name: { $containsi: term } },
+          { performers: { $containsi: term } },
+          { description: { $containsi: term } },
+          { venue: { $containsi: term } },
+        ],
+      },
+    },
+    {
+      encode: false,
+    }
+  )
+
+  const res = await fetch(`${API_URL}/api/events?${query}&populate=*`)
+  const { data } = await res.json()
+
+  return {
+    props: {
+      events: data,
+    },
+  }
+}
