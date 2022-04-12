8c88c90250c766d002887bfc616189c982f57783
Leo <lcheung@swellbottle.com>
2022-04-12 13:44:20 -0400

    populate event single using static

--- frontend/pages/events/[slug].js
+++ frontend/pages/events/[slug].js
@@ -1,7 +1,51 @@
+import Link from 'next/link'
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-function EventPage() {
-  return <Layout title="Event Page">Event Page</Layout>
+export default function EventPage({ evt }) {
+  return (
+    <Layout title="Event Page">
+      <h1>{evt.name}</h1>
+    </Layout>
+  )
 }
 
-export default EventPage
+// generate all events slugs and paths for getStaticProps
+export async function getStaticPaths() {
+  // get all events
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  const paths = events.map((evt) => ({ params: { slug: evt.slug } }))
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+// get events at build time must required getStaticPaths
+export async function getStaticProps({ params: { slug } }) {
+  // console.log(slug)
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const events = await res.json()
+
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1
+  }
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   // console.log(slug)
+//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+//   const events = await res.json()
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   }
+// }
