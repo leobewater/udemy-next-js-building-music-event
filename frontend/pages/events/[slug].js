04c64f8cf6c7610fe856adc2836dd1a3e3bf6754
Leo <lcheung@swellbottle.com>
2022-04-12 11:50:07 -0400

    add jsconfig and update path @

--- frontend/pages/events/[slug].js
+++ frontend/pages/events/[slug].js
@@ -1,4 +1,4 @@
-import Layout from '../../components/Layout'
+import Layout from '@/components/Layout'
 
 function EventPage() {
   return <Layout title="Event Page">Event Page</Layout>
--- frontend/pages/events/add.js
+++ frontend/pages/events/add.js
@@ -1,4 +1,4 @@
-import Layout from '../../components/Layout'
+import Layout from '@/components/Layout'
 
 function AddEventPage() {
   return <Layout title="Add New Event">add Event</Layout>
