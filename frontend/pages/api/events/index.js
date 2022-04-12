69e3a5e12da3f253a92a00aac1d83c8295cbc2f3
Leo <lcheung@swellbottle.com>
2022-04-12 12:01:53 -0400

    populate data to the event api and block all methods unless it's GET

--- frontend/pages/api/events/index.js
+++ frontend/pages/api/events/index.js
@@ -1,5 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// use commonjs for running this on the server side
+const { events } = require('./data.json')
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === 'GET') {
+    res.status(200).json(events)
+  } else {
+    res.setHeader('Allow', ['GET'])
+    res.status(405).json({ message: `Method ${req.method} is not allowed.` })
+  }
 }
