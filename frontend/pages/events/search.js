9c0b8ad65386ac8aeb22133a21a52fb14fb30144
Leo <lcheung@swellbottle.com>
2022-04-13 09:17:20 -0400

    clean up search page import

--- frontend/pages/events/search.js
+++ frontend/pages/events/search.js
@@ -1,9 +1,9 @@
+import qs from 'qs'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem'
 import { API_URL } from '@/config/index'
-import qs from 'qs'
-import { useRouter } from 'next/router'
 
 export default function SearchPage({ events }) {
   const router = useRouter()
