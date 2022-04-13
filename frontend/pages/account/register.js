8b0dc2e56b5570ab34d3aaf4764c5c1e244d8528
Leo <lcheung@swellbottle.com>
2022-04-13 12:49:46 -0400

    set up register and login fields

--- frontend/pages/account/register.js
+++ frontend/pages/account/register.js
@@ -1,7 +1,77 @@
-import React from 'react'
+import { FaUser } from 'react-icons/fa'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useState, useEffect, useContext } from 'react'
+import Link from 'next/link'
+import Layout from '@/components/Layout'
+import styles from '@/styles/AuthForm.module.css'
 
 export default function RegisterPage() {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log({ email, password })
+  }
+
   return (
-    <div>Register</div>
+    <Layout title="User Registration">
+      <div className={styles.auth}>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <ToastContainer />
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="passwordConfirm">Confirm</label>
+            <input
+              type="password"
+              id="passwordConfirm"
+              name="passwordConfirm"
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+            />
+          </div>
+          <input type="submit" value="Login" className="btn" />
+        </form>
+
+        <p>
+          Aready have an account? <Link href="/account/login">Login</Link>
+        </p>
+      </div>
+    </Layout>
   )
 }
