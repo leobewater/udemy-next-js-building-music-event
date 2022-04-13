d09dac0608e727c2c6d4721f12d0ccd6095a5ef1
Leo <lcheung@swellbottle.com>
2022-04-13 09:16:58 -0400

    update about us function

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,13 +1,11 @@
 import Layout from '@/components/Layout'
 
-const AboutPage = () => {
+export default function AboutPage() {
   return (
     <Layout title="About DJ Events" description="About DJ description">
       <h1>About</h1>
-      <p>This is an app to find the latest DJ and other music events</p>
+      <p>This is an app to find the latest DJ and other musical events</p>
       <p>Version: 1.0.0</p>
     </Layout>
   )
 }
-
-export default AboutPage
