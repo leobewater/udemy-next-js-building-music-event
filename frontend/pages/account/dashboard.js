4c555c9f090a476808f9e61a50b07165e72b6d0e
Leo <lcheung@swellbottle.com>
2022-04-14 13:26:01 -0400

    get my events in dashboard

--- frontend/pages/account/dashboard.js
+++ frontend/pages/account/dashboard.js
@@ -1,5 +1,28 @@
+import { parseCookie } from '@/helpers/index'
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function Dashboard() {
+export default function Dashboard({events}) {
   return <Layout title="User Dashboard">Dashboard</Layout>
+}
+
+// when load, server side get my events from strapi
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookie(req)
+  // console.log(token)
+
+  const res = await fetch(`${API_URL}/api/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const events = await res.json()
+
+  return {
+    props: {
+      events,
+    },
+  }
 }
