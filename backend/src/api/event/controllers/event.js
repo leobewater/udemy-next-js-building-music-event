783383edc8b5e1e4c9c3d4cefe61083f02e1b179
Leo <lcheung@swellbottle.com>
2022-04-14 13:56:22 -0400

    add user to event for creation

--- backend/src/api/event/controllers/event.js
+++ backend/src/api/event/controllers/event.js
@@ -1,36 +1,44 @@
-'use strict';
+'use strict'
 
 /**
  *  event controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi').factories
 
 //module.exports = createCoreController('api::event.event');
 
 // add events/me api route
-module.exports = createCoreController("api::event.event", ({ strapi }) => ({
-    // Get logged in users
-    async me(ctx) {
-      const user = ctx.state.user;
-   
-      if (!user) {
-        return ctx.badRequest(null, [
-          { message: "No authorization header was found" },
-        ]);
-      }
-   
-      const data = await strapi.db.query("api::event.event").findMany({
-        where: {
-          user: { id: user.id },
-        },
-        populate: { user: true, image: true },
-      });
-      if (!data) {
-        return ctx.notFound();
-      }
-   
-      const res = await this.sanitizeOutput(data, ctx);
-      return res;
-    },
-  }));
+module.exports = createCoreController('api::event.event', ({ strapi }) => ({
+  // create event with linked user
+  async create(ctx) {
+    let entity
+    ctx.request.body.data.user = ctx.state.user
+    entity = await strapi.service('api::event.event').create(ctx.request.body)
+    return entity
+  },
+  
+  // Get logged in users
+  async me(ctx) {
+    const user = ctx.state.user
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { message: 'No authorization header was found' },
+      ])
+    }
+
+    const data = await strapi.db.query('api::event.event').findMany({
+      where: {
+        user: { id: user.id },
+      },
+      populate: { user: true, image: true },
+    })
+    if (!data) {
+      return ctx.notFound()
+    }
+
+    const res = await this.sanitizeOutput(data, ctx)
+    return res
+  },
+}))
