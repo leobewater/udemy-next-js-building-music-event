193bc23634eae8b5b613f8d5dc41c877c54f9b60
Leo <lcheung@swellbottle.com>
2022-04-14 11:43:55 -0400

    redirect to dashboard after user logged in

--- frontend/context/AuthContext.js
+++ frontend/context/AuthContext.js
@@ -8,6 +8,8 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [error, setError] = useState(null)
 
+  const router = useRouter()
+
   useEffect(() => {
     checkUserLoggedIn()
   }, [])
@@ -50,6 +52,7 @@ export const AuthProvider = ({ children }) => {
 
     if (res.ok) {
       setUser(data.user)
+      router.push('/account/dashboard')
     } else {
       setUser(null)
     }
---
+++ frontend/pages/account/dashboard.js
@@ -0,0 +1,5 @@
+import Layout from '@/components/Layout'
+
+export default function Dashboard() {
+  return <Layout title="User Dashboard">Dashboard</Layout>
+}
