83b7bda761fa6ae99bbf049fcfa2f763082fb960
Leo <lcheung@swellbottle.com>
2022-04-14 09:29:06 -0400

    add menu items with auth

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 import Search from './Search'
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import AuthContext from '@/context/AuthContext'
+import { useContext } from 'react'
 import styles from '@/styles/Header.module.css'
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext)
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -21,26 +25,45 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a>Add Event</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/login">
-              <a className='btn-secondary btn-icon'><FaSignInAlt />Login</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/login">
-              <a className='btn-secondary btn-icon'><FaSignOutAlt />Logout</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/register">
-              <a>Register</a>
-            </Link>
-          </li>
+          {user ? (
+            // if logged in
+            <>
+              <li>
+                <Link href="/events/add">
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/account/dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className="btn-secondary btn-icon"
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            // if logged out
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/account/register">
+                  <a>Register</a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
