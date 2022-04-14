714ea439bd5e3dc0f01ef5e7d2c49df7df38a70c
Leo <lcheung@swellbottle.com>
2022-04-14 14:59:47 -0400

    update server.js

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
-  proxy: true,
   url: env('MY_HEROKU_URL'),
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      'DATABASE_URL',
+      'HEROKU_POSTGRESQL_MAROON_URL',
+    ]),
   },
 })
